736c462272043978f28a2e94104674473c0b72d2
unknown <abdelhameda410@gmail.com>
2025-06-17 19:40:59 +0300

    err

--- src/Components/CustomCheckbox.jsx
+++ src/Components/CustomCheckbox.jsx
@@ -7,35 +7,5 @@ export const CustomCheckbox = ({ label, control, name }) => {
 
   const { handleSubmit } = useForm();
 
-  return (
-    <div>
-      {/* <Controller
-        control={control}
-        name={name}
-        rules={{ required: true }}
-        render={({ field: { onChange, onBlur, value, ref } }) => (
-          <div className="flex items-center gap-1">
-            <Checkbox
-              onBlur={onBlur}
-              onChange={onChange}
-              checked={value}
-              inputRef={ref}
-              className="border-black cursor-pointer"
-            />
-            <div className="flex items-center gap-2 w-fit">
-              <label htmlFor={label} className="text-sm font-[500] select-none">
-                {label}
-              </label>
-            </div>
-
-            {!isChecked && (
-              <span className="text-sm text-red-500 flex mt-3">
-                You must agree to the terms
-              </span>
-            )}
-          </div>
-        )}
-      /> */}
-    </div>
-  );
+  return <div></div>;
 };
