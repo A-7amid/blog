e8403a96253387e896b070449e6c196293f52256
unknown <abdelhameda410@gmail.com>
2025-06-17 19:44:13 +0300

    err 2

--- src/Components/CustomCheckbox.jsx
+++ src/Components/CustomCheckbox.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Checkbox } from "@/components/ui/checkbox";
+import { Checkbox } from "@/Components/ui/checkbox";
 
 export const CustomCheckbox = ({ label, control, name }) => {
   const [isChecked, setIsChecked] = useState(false);
